288dc517916fbb10d2273d511bb8685e65d1f838
Sashko Stubailo <sashko@stubailo.com>
2016-03-01 10:52:06 -0800

    Clean up, redirect /

--- index.js
+++ index.js
@@ -4,6 +4,10 @@ import express from 'express';
 
 const app = express();
 
+app.get('/', function(req, res) {
+  res.redirect('/graphql');
+});
+
 app.use('/graphql', graphqlHTTP({ schema: Schema, graphiql: true }));
 
 app.listen(3000);
